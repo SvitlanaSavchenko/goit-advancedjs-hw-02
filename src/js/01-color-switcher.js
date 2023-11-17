b0a4e22ae47e773b47a5af0cf5f12593754a76ff
SvitlanaSavchenko <134533907+SvitlanaSavchenko@users.noreply.github.com>
2023-11-17 21:21:02 +0100

    task1-done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
-console.log('hello world');
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const body = document.querySelector('body');
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+let intervalId;
+let isRunning = false;
+
+startButton.addEventListener('click', () => {
+  if (isRunning) {
+    return;
+  }
+
+  isRunning = true;
+  startButton.disabled = true;
+
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  isRunning = false;
+  startButton.disabled = false;
+
+  // Clear the interval using the stored interval ID
+  clearInterval(intervalId);
+});
